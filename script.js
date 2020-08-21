83ab707ed0d1c431373fdbbae9f193d28ca773c9
David <chen7david@me.com>
2020-08-22 00:29:19 +0800

    update

---
+++ script.js
@@ -0,0 +1,29 @@
+const dd = (val) => console.log(val)
+const body = document.querySelector('.background')
+const boxes = document.querySelectorAll('.box')
+
+boxes.forEach(box => {
+    box.addEventListener('click', function () {
+        box.style.backgroundColor = 'black'
+        box.innerHTML = '0'
+    })
+})
+
+function rand(limit){
+    return Math.floor(Math.random() * limit)
+}
+
+let counter = 0
+
+function changeColor(){
+    if(counter >= boxes.length) counter = 0
+    const box = boxes[counter]
+    const color = `rgb(${rand(255)},${rand(255)},${rand(255)})`
+    box.style.backgroundColor = color
+    body.style.backgroundColor = color
+    box.innerHTML = rand(10)
+    counter++
+}
+
+body.addEventListener('click', changeColor)
+
